c24869f4d04e5cb5de09ea0dea856ca71fc9b76d
marcelo-sl <mar.sousa2061@gmail.com>
2021-10-27 23:15:51 -0300

    Initial commit

---
+++ SerilogDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerilogDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("You requested the index page.");
+
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i == 56)
+                    {
+                        throw new Exception("This is our demo exception");
+                    } else
+                    {
+                        _logger.LogInformation("The value of i is {i}", i);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "We caught this exception in the index get call.");
+            }
+        }
+    }
+}
